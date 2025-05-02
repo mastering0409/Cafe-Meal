df174e80ecee0875a50d34d460b48294c80543f0
reliableengineer0308 <reliableengineer0308@gmail.com>
2025-05-02 19:25:38 -0400

    updated one thing

--- src/utils/env.js
+++ src/utils/env.js
@@ -8,6 +8,7 @@ import { Platform } from 'react-native';
 const liveHost = 'https://us-central1-yummeals-e8d3d.cloudfunctions.net';
 const localHost = 'http://localhost:5001/yummeals-e8d3d/us-central1';
 
+
 export const isAndroid = Platform.OS === 'android';
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const isMock = false; // Change isMock to true during development to limit API requests to Google
